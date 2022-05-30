ef464bfb471c26990c27b3c44bf838a010fe27f9
Sakuntha Hansaka <e17340@eng.pdn.ac.lk>
2022-05-31 03:26:26 +0530

    remove unused dependencies

--- lib/Event_1/store.dart
+++ lib/Event_1/store.dart
@@ -1,9 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
