8c936a4e828dfd1d1c37b58966905d85375aacf2
Sakuntha Hansaka <e17340@eng.pdn.ac.lk>
2022-10-23 00:11:00 +0530

    AppCheck package added to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
@@ -12,6 +13,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await FlutterDownloader.initialize(debug: true);
+  await FirebaseAppCheck.instance.activate(
+    webRecaptchaSiteKey: 'recaptcha-v3-site-key',
+  );
   runApp(
     ChangeNotifierProvider(
       create: (context) => PlayStoreProvider(),
