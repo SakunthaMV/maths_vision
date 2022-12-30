83168a80c1cca2e84ee3b792ba4fc1d4f1d5f059
Sakuntha Hansaka <e17340@eng.pdn.ac.lk>
2022-12-31 04:58:34 +0530

    add a reminder for future work.

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'Providers/google_sign_in_provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // TODO: remove debug things before release app.
   await FlutterDownloader.initialize(debug: true);
   await FirebaseAppCheck.instance.activate(
     webRecaptchaSiteKey: 'recaptcha-v3-site-key',
