ee6e829c339b5c6dfc8ac9c4cb0b3a853c285ecb
Sakuntha Hansaka <e17340@eng.pdn.ac.lk>
2022-12-31 22:22:02 +0530

    this is admob ad helper and this branch continues with admob ads

--- lib/Services/ad_manager.dart
+++ lib/Services/ad_manager.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 class AdManager {
-
   static String get appId {
     if (Platform.isAndroid) {
       return "ca-app-pub-2445637094519371~9400624945";
@@ -9,7 +8,6 @@ class AdManager {
       throw new UnsupportedError("Unsupported platform");
     }
   }
-
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
       return "ca-app-pub-2445637094519371/1088087280";
@@ -17,7 +15,6 @@ class AdManager {
       throw new UnsupportedError("Unsupported platform");
     }
   }
-
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
       return "ca-app-pub-2445637094519371/4015834653";
@@ -25,7 +22,6 @@ class AdManager {
       throw new UnsupportedError("Unsupported platform");
     }
   }
-
   static String get rewardedAdUnitId {
     if (Platform.isAndroid) {
       return "ca-app-pub-2445637094519371/2983908298";
