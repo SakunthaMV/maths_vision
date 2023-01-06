d366eb7e478b7a50178b3ec3223c64354636dc67
Sakuntha Hansaka <e17340@eng.pdn.ac.lk>
2023-01-06 13:28:47 +0530

    add every ad type which are used inside the app with test ad id.

--- lib/Services/ad_manager.dart
+++ lib/Services/ad_manager.dart
@@ -3,28 +3,70 @@ import 'dart:io';
 class AdManager {
   static String get appId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-2445637094519371~9400624945";
+      return "ca-app-pub-3940256099942544~3347511713";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
   }
-  static String get bannerAdUnitId {
+  static String get bannerNote {
     if (Platform.isAndroid) {
-      return "ca-app-pub-2445637094519371/1088087280";
+      return "ca-app-pub-3940256099942544/6300978111";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
   }
-  static String get interstitialAdUnitId {
+  static String get bannerAnswerDisplay {
     if (Platform.isAndroid) {
-      return "ca-app-pub-2445637094519371/4015834653";
+      return "ca-app-pub-3940256099942544/6300978111";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
   }
-  static String get rewardedAdUnitId {
+  static String get bannerEventQuestion {
     if (Platform.isAndroid) {
-      return "ca-app-pub-2445637094519371/2983908298";
+      return "ca-app-pub-3940256099942544/6300978111";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+  static String get bannerFullPaper {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/6300978111";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+  static String get bannerMarkingScheme {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/6300978111";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+  static String get interstitialLevelUp {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/1033173712";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+  static String get interstitialFullPaper {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/1033173712";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+  static String get interstitialLessonVideo {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/1033173712";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+  static String get rewardedAnswerDisplay {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/5224354917";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
