94fdbc59aacdfb4e7e75a8a372b6ad31b1365313
Sakuntha Hansaka <e17340@eng.pdn.ac.lk>
2023-01-08 22:49:46 +0530

    update test function with error massages.

--- lib/Services/cloud_functions.dart
+++ lib/Services/cloud_functions.dart
@@ -3,7 +3,13 @@ import 'package:cloud_functions/cloud_functions.dart';
 class CloudFunctions {
   static Future<void> testFunction() async {
     HttpsCallable callable = FirebaseFunctions.instance.httpsCallable('checkFunction');
-    final response = await callable.call();
-    print(response.data);
+    try {
+      final response = await callable.call();
+      print(response.data);
+    } on FirebaseFunctionsException catch (e) {
+      print('Firebase Error: ${e.toString()}');
+    } catch (e) {
+      print(e.toString());
+    }
   }
 }
