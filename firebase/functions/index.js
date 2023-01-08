d6cb8d38a78fbb2d0f0bfaeec9896e8b7aef8cb1
Sakuntha Hansaka <e17340@eng.pdn.ac.lk>
2023-01-08 17:26:20 +0530

    add cloud function for capitalize first letter of first name and last name

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -4,5 +4,33 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.checkFunction = functions.https.onCall(async (data, context) => {
-    return "Function Work Correctly."
+    // return new Promise((resolve) => setTimeout(resolve, 5000)).then(async () => {
+    //     return admin.firestore().collection('Users').doc('023fq5MpXbhPQuLmVPxCDgN7nAF3').get().then((doc) => {
+    //         const data = doc.data().User_Details.firstName;
+    //         return data;
+    //     });
+    // });
+    return 'hello';
+});
+
+exports.checkNameCase = functions.auth.user().onCreate(async (user) => {
+    return new Promise((resolve) => setTimeout(resolve, 5000)).then(async () => {
+        return admin.firestore().collection('Users').doc(user.uid).get().then((doc) => {
+            let firstName = doc.data().User_Details.firstName;
+            let lastName = doc.data().User_Details.lastName;
+            if (firstName[0] !== firstName[0].toUpperCase() || lastName[0] !== lastName[0].toUpperCase()) {
+                if (firstName[0] !== firstName[0].toUpperCase()) {
+                    firstName = firstName[0].toUpperCase() + firstName.slice(1);
+                }
+                if (lastName[0] !== lastName[0].toUpperCase()) {
+                    lastName = lastName[0].toUpperCase() + lastName.slice(1);
+                }
+                return admin.firestore().collection('Users').doc(user.uid).update({
+                    'User_Details.firstName': firstName,
+                    'User_Details.lastName': lastName,
+                });
+            }
+            return null;
+        });
+    });
 });
